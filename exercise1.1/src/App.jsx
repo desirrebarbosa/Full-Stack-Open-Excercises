1fbde74cd8d938afb111ceb305d686f8d0ebe34f
Desirre Barbosa <desblssbarbosa@gmail.com>
2025-09-09 14:20:50 +0800

    added missing spaces

--- exercise1.1/src/App.jsx
+++ exercise1.1/src/App.jsx
@@ -10,13 +10,13 @@ const App = () => {
     <div>
       <h1>{course}</h1>
       <p>
-        {part1}{exercises1}
+        {part1} {exercises1}
       </p>
       <p>
-        {part2}{exercises2}
+        {part2} {exercises2}
       </p>
       <p>
-        {part3}{exercises3}
+        {part3} {exercises3}
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
