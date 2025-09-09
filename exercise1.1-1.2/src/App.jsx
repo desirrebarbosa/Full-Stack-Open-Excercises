d93102c3f8abd803201d5c10a34c127611d2464d
Desirre Barbosa <desblssbarbosa@gmail.com>
2025-09-09 14:06:19 +0800

    finished app.jsx

--- exercise1.1-1.2/src/App.jsx
+++ exercise1.1-1.2/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const par2 = 'Using props to pass data'
+  const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
-  const exercise3 = 14
+  const exercises3 = 14
   return(
     <div>
       <h1>{course}</h1>
-      
+      <p>
+        {part1}{exercises1}
+      </p>
+      <p>
+        {part2}{exercises2}
+      </p>
+      <p>
+        {part3}{exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
